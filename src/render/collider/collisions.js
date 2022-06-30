d730289ea51c61f54ef4560d23b94fb2546c7c57
ltbringer <amresh.venugopal@gmail.com>
2022-07-01 04:22:35 +0530

    refactor: path

--- src/render/collider/collisions.js
+++ src/render/collider/collisions.js
@@ -1,5 +1,5 @@
 import { chunk } from "lodash";
-import collisions from "./data/collision.json";
+import collisions from "../../data/collision.json";
 
 export const makeCollisionGrid = () => {
     return chunk(collisions, 40)

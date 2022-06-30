fada7a320810daa14ba17b7e0d06c0253f22e2d5
ltbringer <amresh.venugopal@gmail.com>
2022-07-01 04:25:55 +0530

    feat: collision detection.

--- src/render/animation.js
+++ src/render/animation.js
@@ -1,25 +1,61 @@
 import { getCtx } from "./canvas";
-import BgImage from "../img/november.png";
-import PlayerSprite from "../img/main_char_sprite.png";
-import { CHAR_WIDTH, CHAR_HEIGHT, CHAR_X_OFFSET, CHAR_Y_OFFSET } from "../constants";
+import { PLAYER_MOVESPEED } from "../constants";
+import { getColliders } from "./collider";
 
 
-export const animationBuilder = ({ bg, bgImg, playerImg, state }) => {
-    bgImg.src = BgImage;
-    playerImg.src = PlayerSprite;
-    const ctx = getCtx();
-    bgImg.onload = () => {
-        bg.draw(ctx);
-        playerImg.onload = () => {
-            ctx.drawImage(playerImg, 0, 0, CHAR_WIDTH, CHAR_HEIGHT, CHAR_X_OFFSET, CHAR_Y_OFFSET, CHAR_WIDTH * 4, CHAR_HEIGHT * 4);
+const checkCollision = (player, collider, { x, y }) => {
+    const playerPosition = player.position;
+    const colliderPosition = collider.position;
+
+    return ((playerPosition.x + player.width > colliderPosition.x + x) // player is to the right of collider
+    && (playerPosition.x < colliderPosition.x + x + collider.width) // player is to the left of collider
+    && (playerPosition.y + player.height > colliderPosition.y + y) // player is below collider
+    && (playerPosition.y < colliderPosition.y + y + collider.height)) // player is above collider
+}
+
+const motionControl = ({ state, bg, colliders, player }) => {
+    const { keys } = state;
+    let futureKeyState = {x: 0, y: 0}
+    if (keys.isPressed(keys.up)) {
+        futureKeyState.y = PLAYER_MOVESPEED;
+    } else if (keys.isPressed(keys.left)) {
+        futureKeyState.x = PLAYER_MOVESPEED;
+    } else if (keys.isPressed(keys.down)) {
+        futureKeyState.y = -PLAYER_MOVESPEED;
+    } else if (keys.isPressed(keys.right)) {
+        futureKeyState.x = -PLAYER_MOVESPEED;
+    }
+
+    const collisions = colliders.some(collider => checkCollision(player, collider, futureKeyState));
+
+    const moveMobile = (mobile) => {
+        if (keys.isPressed(keys.up) && !collisions) {
+            mobile.position.y += PLAYER_MOVESPEED
+        } else if (keys.isPressed(keys.left) && !collisions) {
+            mobile.position.x += PLAYER_MOVESPEED
+        } else if (keys.isPressed(keys.down) && !collisions) {
+            mobile.position.y -= PLAYER_MOVESPEED
+        } else if (keys.isPressed(keys.right) && !collisions) {
+            mobile.position.x -= PLAYER_MOVESPEED
         }
     }
 
+    [...colliders, bg].forEach(moveMobile);
+}
+
+export const animationBuilder = ({ bg, player, state }) => {
+    const ctx = getCtx();
+    const sprites = [bg, player];
+    sprites.forEach(sprite => sprite.loadImage(ctx));
+    const colliders = getColliders();
+    console.log(player.position.x, player.position.x + player.width);
+
     const animate = () => {
         bg.draw(ctx);
-        ctx.drawImage(playerImg, 0, 0, CHAR_WIDTH, CHAR_HEIGHT, CHAR_X_OFFSET, CHAR_Y_OFFSET, CHAR_WIDTH * 4, CHAR_HEIGHT * 4);
+        colliders.forEach(collider => collider.draw(ctx));
+        player.draw(ctx);
         window.requestAnimationFrame(animate);
-        bg.update(state.keys)
+        motionControl({ state, bg, colliders, player });
     }
     return animate;
 }

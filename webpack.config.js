4d58ff52113ba3c026c3e008726f575bd686a04a
ltbringer <amresh.venugopal@gmail.com>
2022-06-30 16:28:31 +0530

    add: support for images.

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   devtool: 'inline-source-map',

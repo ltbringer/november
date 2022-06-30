608a7ce61e38b65ddc2639773c31e9e5b45dee26
ltbringer <amresh.venugopal@gmail.com>
2022-07-01 04:24:12 +0530

    update: state is a class.

--- src/state/index.js
+++ src/state/index.js
@@ -1,8 +1,40 @@
-export const state = {
-    keys: {
-        w: false,
-        a: false,
-        s: false,
-        d: false
+export class Keys {
+    constructor({ up, down, left, right }) {
+        this.up = up
+        this.down = down
+        this.left = left
+        this.right = right
+        this.pressed = {
+            [this.up]: false,
+            [this.down]: false,
+            [this.left]: false,
+            [this.right]: false
+        }
+        this.reverse = {
+            [this.up]: this.down,
+            [this.down]: this.up,
+            [this.left]: this.right,
+            [this.right]: this.left
+        }
+        this.lastKey = null;
+    }
+
+    press(key) {
+        this.pressed[key] = true;
+        this.lastKey = key;
+    }
+
+    release(key) {
+        this.pressed[key] = false;
+    }
+
+    isPressed(direction) {
+        return this.pressed[direction];
+    }
+}
+
+export class State {
+    constructor({ keys }) {
+        this.keys = new Keys(keys)
     }
 }

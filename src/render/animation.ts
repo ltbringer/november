c9f5cd1549df36c64baf45d13f17ff6e8701ce8b
ltbringer <amresh.venugopal@gmail.com>
2022-07-02 17:53:24 +0530

    add: foreground.

--- src/render/animation.ts
+++ src/render/animation.ts
@@ -5,10 +5,10 @@ import { BoxCollider } from "./collider";
 import { State } from "../state";
 import { Sprite } from "../sprites";
 
-type MotionControlArgs = { state: State, bg: Sprite, colliders: BoxCollider[], player: Sprite}
-type AnimationBuilderArgs = { bg: Sprite, player: Sprite, state: State, canvas: HTMLCanvasElement}
+type MotionControlArgs = { state: State, bg: Sprite, player: Sprite, fg: Sprite, colliders: BoxCollider[],}
+type AnimationBuilderArgs = { bg: Sprite, player: Sprite, fg: Sprite, state: State, canvas: HTMLCanvasElement}
 
-const motionControl = ({ state, bg, colliders, player }: MotionControlArgs): void => {
+const motionControl = ({ state, bg, player, fg, colliders }: MotionControlArgs): void => {
     const { keys } = state;
     let futureKeyState: coordinates = {x: 0, y: 0}
     if (keys.isPressed(keys.up)) {
@@ -26,30 +26,36 @@ const motionControl = ({ state, bg, colliders, player }: MotionControlArgs): voi
     const moveMobile = (mobile: BoxCollider | Sprite) => {
         if (keys.isPressed(keys.up) && !collisions) {
             mobile.position.y += PLAYER_MOVESPEED
+            player.animate("up");
         } else if (keys.isPressed(keys.left) && !collisions) {
             mobile.position.x += PLAYER_MOVESPEED
+            player.animate("left");
         } else if (keys.isPressed(keys.down) && !collisions) {
             mobile.position.y -= PLAYER_MOVESPEED
+            player.animate("down");
         } else if (keys.isPressed(keys.right) && !collisions) {
             mobile.position.x -= PLAYER_MOVESPEED
+            player.animate("right");
         }
     }
 
-    [...colliders, bg].forEach(moveMobile);
+    [...colliders, bg, fg].forEach(moveMobile);
 }
 
-export const animationBuilder = ({ bg, player, state, canvas }: AnimationBuilderArgs): () => void => {
+export const animationBuilder = ({ bg, player, state, canvas, fg }: AnimationBuilderArgs): () => void => {
     const ctx: CanvasRenderingContext2D = getCtx(canvas);
-    const sprites: Sprite[] = [bg, player];
+    const sprites: Sprite[] = [bg, player, fg];
     sprites.forEach(sprite => sprite.loadImage(ctx));
     const colliders = getColliders();
 
     const animate = (): void => {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         bg.draw(ctx);
         colliders.forEach(collider => collider.draw(ctx));
         player.draw(ctx);
+        fg.draw(ctx);
         window.requestAnimationFrame(animate);
-        motionControl({ state, bg, colliders, player });
+        motionControl({ state, bg, player, fg, colliders });
     }
     return animate;
 }

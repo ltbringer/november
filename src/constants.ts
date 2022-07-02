16c092b6706812d8571e27a10ea16301ba87eb9c
ltbringer <amresh.venugopal@gmail.com>
2022-07-02 17:49:21 +0530

    update: increase player movespeed.

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const MAP_Y_OFFSET: number = -900;
 export const CHAR_X_OFFSET: number = 600;
 export const CHAR_Y_OFFSET: number = 600;
 export const IS_COLLIDER: number = 955;
-export const PLAYER_MOVESPEED: number = 2;
+export const PLAYER_MOVESPEED: number = 4;
 export const TILES: { rows: number, cols: number } = {
     rows: 40,
     cols: 30,

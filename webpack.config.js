11180a317b03a01b4382d9ed4806566bb13bde77
ltbringer <amresh.venugopal@gmail.com>
2022-07-02 18:38:22 +0530

    add: env to build args.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,43 @@
 const path = require('path');
 const glob = require('glob');
 
-module.exports = {
-  entry: {
-    ts: glob.sync("./src/**/*.ts"),  
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules|\.d\.ts$/
-      },
-      {
-          test: /\.d\.ts$/,
-          loader: 'ignore-loader'
-      }
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  devtool: 'inline-source-map',
-  mode: "development",
-  devServer: {
-    static: "./dist",
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+module.exports = env => {
+  return {
+    entry: {
+      ts: glob.sync("./src/**/*.ts"),  
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules|\.d\.ts$/
+        },
+        {
+            test: /\.d\.ts$/,
+            loader: 'ignore-loader'
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    devtool: 'inline-source-map',
+    mode: env === "production" ? "production" : "development",
+    devServer: {
+      static: "./dist",
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    }
   }
 };

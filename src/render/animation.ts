0083dd5a6f24734fa954ba0e9f4e52d3c6dcf47d
ltbringer <amresh.venugopal@gmail.com>
2022-07-10 01:58:40 +0530

    add: controls for enemy npc.

--- src/render/animation.ts
+++ src/render/animation.ts
@@ -1,14 +1,15 @@
 import { getCtx } from "./canvas";
-import { PLAYER_MOVESPEED } from "../constants";
+import { ENEMY_CHASE_DISTANCE, PLAYER_MOVESPEED } from "../constants";
 import { getColliders, checkCollision } from "./collider";
 import { BoxCollider } from "./collider";
 import { State } from "../state";
-import { Sprite } from "../sprites";
+import { Sprite, Playable } from "../sprites";
+import { manhattanDistance } from "../utils/misc";
 
-type MotionControlArgs = { state: State, bg: Sprite, player: Sprite, fg: Sprite, colliders: BoxCollider[],}
-type AnimationBuilderArgs = { bg: Sprite, player: Sprite, fg: Sprite, state: State, canvas: HTMLCanvasElement}
+type MotionControlArgs = { state: State, bg: Sprite, player: Playable, enemy: Playable, fg: Sprite, colliders: BoxCollider[],}
+type AnimationBuilderArgs = { bg: Sprite, player: Playable, enemy: Playable, fg: Sprite, state: State, canvas: HTMLCanvasElement}
 
-const motionControl = ({ state, bg, player, fg, colliders }: MotionControlArgs): void => {
+const motionControl = ({ state, bg, player, enemy, fg, colliders }: MotionControlArgs): void => {
     const { keys } = state;
     let futureKeyState: coordinates = {x: 0, y: 0}
     if (keys.isPressed(keys.up)) {
@@ -21,40 +22,53 @@ const motionControl = ({ state, bg, player, fg, colliders }: MotionControlArgs):
         futureKeyState.x = -PLAYER_MOVESPEED;
     }
 
-    const collisions = colliders.some((collider: BoxCollider) => checkCollision(player, collider, futureKeyState));
+    const playerCollisions = colliders.some((collider: BoxCollider) => checkCollision(player, collider, futureKeyState));
+    const enemyCollisions = colliders.some((collider: BoxCollider) => checkCollision(enemy, collider, futureKeyState));
 
     const moveMobile = (mobile: BoxCollider | Sprite) => {
-        if (keys.isPressed(keys.up) && !collisions) {
+        if (keys.isPressed(keys.up) && !playerCollisions) {
             mobile.position.y += PLAYER_MOVESPEED
             player.animate("up");
-        } else if (keys.isPressed(keys.left) && !collisions) {
+        } else if (keys.isPressed(keys.left) && !playerCollisions) {
             mobile.position.x += PLAYER_MOVESPEED
             player.animate("left");
-        } else if (keys.isPressed(keys.down) && !collisions) {
+        } else if (keys.isPressed(keys.down) && !playerCollisions) {
             mobile.position.y -= PLAYER_MOVESPEED
             player.animate("down");
-        } else if (keys.isPressed(keys.right) && !collisions) {
+        } else if (keys.isPressed(keys.right) && !playerCollisions) {
             mobile.position.x -= PLAYER_MOVESPEED
             player.animate("right");
         }
     }
 
-    [...colliders, bg, fg].forEach(moveMobile);
+    const enemyDistance = manhattanDistance(player.position, enemy.position);
+    if (enemyDistance <= ENEMY_CHASE_DISTANCE && !enemyCollisions) {
+        [...colliders, bg, fg].forEach(moveMobile);
+        enemy.follow(player.position, enemyCollisions);
+    } else if (checkCollision(enemy, player, { x: 0, y: 0 })) {
+        enemy.attack(player, true);
+        player.attack(enemy, keys.isPressed("y"));
+    } else {
+        [...colliders, bg, fg, enemy].forEach(moveMobile);
+    }
 }
 
-export const animationBuilder = ({ bg, player, state, canvas, fg }: AnimationBuilderArgs): () => void => {
+export const animationBuilder = ({ bg, player, state, canvas, fg, enemy }: AnimationBuilderArgs): () => void => {
     const ctx: CanvasRenderingContext2D = getCtx(canvas);
-    const sprites: Sprite[] = [bg, player, fg];
+    const sprites: Sprite[] = [bg, player, fg, enemy];
     sprites.forEach(sprite => sprite.loadImage(ctx));
     const colliders = getColliders();
 
     const animate = (): void => {
+        ctx.fillStyle = "#000";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         bg.draw(ctx);
         player.draw(ctx);
+        enemy.draw(ctx);
         fg.draw(ctx);
+        colliders.forEach(collider => collider.draw(ctx));
         window.requestAnimationFrame(animate);
-        motionControl({ state, bg, player, fg, colliders });
+        motionControl({ state, bg, player, enemy, fg, colliders });
     }
     return animate;
 }

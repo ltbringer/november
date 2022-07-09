68f68d228c802dbaedad8a7a984d6e9ce1810bd6
ltbringer <amresh.venugopal@gmail.com>
2022-07-10 01:56:02 +0530

    add: manhattan distance and randomInt.

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,12 @@
 export const hasKey = <O>(obj: O, key: PropertyKey): key is keyof O => {
     return key in obj
 };
+
+
+export const manhattanDistance = (a: coordinates, b: coordinates): number => {
+    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
+}
+
+export const randomInt = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}

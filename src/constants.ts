9377c9dc067752655125bc32a463262b4c02f699
ltbringer <amresh.venugopal@gmail.com>
2022-07-10 01:59:15 +0530

    add: constants for hp, mana bar.

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,16 @@ export const CHAR_WIDTH: number = 16;
 export const CHAR_HEIGHT: number = 16;
 export const MAP_X_OFFSET: number = -600;
 export const MAP_Y_OFFSET: number = -900;
+
 export const CHAR_X_OFFSET: number = 600;
 export const CHAR_Y_OFFSET: number = 600;
+export const BAR_OFFSET: number = 2;
+export const BAR_HEIGHT: number = 10;
+
+export const ENEMY_X_OFFSET: number = 265;
+export const ENEMY_Y_OFFSET: number = 20;
+export const ENEMY_CHASE_DISTANCE: number = CHAR_WIDTH * 20;
+
 export const IS_COLLIDER: number = 955;
 export const PLAYER_MOVESPEED: number = 4;
 export const TILES: { rows: number, cols: number } = {

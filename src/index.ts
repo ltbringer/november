fb55a36759e9f430d688997eeab551af61af883c
ltbringer <amresh.venugopal@gmail.com>
2022-07-10 01:58:53 +0530

    add: config for player stats and enemy npc.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
-import { Sprite } from "./sprites";
+import { Sprite, Playable } from "./sprites";
 import { mapSetup } from "./render/canvas";
 import { configureKeyPress } from "./events/keys";
 import { animationBuilder } from "./render/animation";
-import { MAP_WIDTH, MAP_HEIGHT, MAP_X_OFFSET, MAP_Y_OFFSET, CHAR_X_OFFSET, CHAR_Y_OFFSET } from "./constants";
+import { MAP_WIDTH, MAP_HEIGHT, MAP_X_OFFSET, MAP_Y_OFFSET, CHAR_X_OFFSET, CHAR_Y_OFFSET, ENEMY_X_OFFSET, ENEMY_Y_OFFSET } from "./constants";
 import { State } from "./state";
 
 import "./css/style.css";
 import BackgroundSprite from "./img/november.png";
 import ForegroundSprite from "./img/november_foreground.png";
 import PlayerSprite from "./img/main_char_sprite.png";
+import EnemySprite from "./img/mushroom_boss.png";
 
 
 const main = () => {
     const playerImg = new Image();
     const bgImg = new Image();
     const fgImg = new Image();
+    const enemyImg = new Image();
+
     bgImg.src = BackgroundSprite;
     playerImg.src = PlayerSprite;
     fgImg.src = ForegroundSprite;
+    enemyImg.src = EnemySprite;
+
     const playerFrames = 16;
 
     const bg = new Sprite({
@@ -50,7 +55,7 @@ const main = () => {
             right: 0
         }
     })
-    const player = new Sprite({
+    const player = new Playable({
         position: {
             x: CHAR_X_OFFSET,
             y: CHAR_Y_OFFSET
@@ -66,11 +71,28 @@ const main = () => {
             left: 8,
             right: 12
         }
-    })
+    }, { hp: 100, mana: 100, muscle: 30, magik: 30, armour: 30, immunity: 30 });
+    const enemy = new Playable({
+        position: {
+            x: ENEMY_X_OFFSET,
+            y: ENEMY_Y_OFFSET
+        },
+        image: enemyImg,
+        width: enemyImg.width,
+        height: enemyImg.height,
+        zoom: 4,
+        directions: {
+            down: 0,
+            up: 0,
+            left: 0,
+            right: 0
+        }
+    }, { hp: 80, mana: 100, muscle: 10, magik: 30, armour: 10, immunity: 30 });
+
     const state = new State({ keys: { up: "w", down: "s", left: "a", right: "d" }});
     const canvas = mapSetup(MAP_WIDTH, MAP_HEIGHT);
     configureKeyPress(state.keys);
-    animationBuilder({ bg, player, state, canvas, fg })();
+    animationBuilder({ bg, player, state, canvas, fg, enemy })();
 }
 
 

2f7adbd1db8fedde1f7f512b1e08e34576bc5373
ltbringer <amresh.venugopal@gmail.com>
2022-07-18 03:30:40 +0530

    add: constants.

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const MAP_WIDTH: number = 1920;
 export const MAP_HEIGHT: number = 1440;
+export const TILE_WIDTH: number = 64;
+export const TILE_HEIGHT: number = 64;
 export const CHAR_WIDTH: number = 16;
 export const CHAR_HEIGHT: number = 16;
 export const MAP_X_OFFSET: number = -600;
@@ -12,7 +14,7 @@ export const BAR_HEIGHT: number = 10;
 
 export const ENEMY_X_OFFSET: number = 265;
 export const ENEMY_Y_OFFSET: number = 20;
-export const ENEMY_CHASE_DISTANCE: number = CHAR_WIDTH * 20;
+export const ENEMY_CHASE_DISTANCE: number = CHAR_WIDTH * 50;
 
 export const IS_COLLIDER: number = 955;
 export const PLAYER_MOVESPEED: number = 4;
@@ -20,3 +22,5 @@ export const TILES: { rows: number, cols: number } = {
     rows: 40,
     cols: 30,
 }
+
+export const DIRECTIONS = [[-1, 0], [0, -1], [1, 0], [0, 1]];

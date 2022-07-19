09f89a068fdf6fe749f8c0c3612fac3c8c43c993
ltbringer <amresh.venugopal@gmail.com>
2022-07-20 03:58:09 +0530

    style: lint.

--- src/render/animation.ts
+++ src/render/animation.ts
@@ -6,73 +6,107 @@ import { State } from "../state";
 import { Sprite, Playable } from "../sprites";
 import { hasKey, manhattanDistance } from "../utils/misc";
 
-type MotionControlArgs = { ctx: CanvasRenderingContext2D, state: State, bg: Sprite, player: Playable, enemy: Playable, fg: Sprite, colliders: BoxCollider[]}
-type AnimationBuilderArgs = { bg: Sprite, player: Playable, enemy: Playable, fg: Sprite, state: State, canvas: HTMLCanvasElement}
+type MotionControlArgs = {
+  ctx: CanvasRenderingContext2D;
+  state: State;
+  bg: Sprite;
+  player: Playable;
+  enemy: Playable;
+  fg: Sprite;
+  colliders: BoxCollider[];
+};
+type AnimationBuilderArgs = {
+  bg: Sprite;
+  player: Playable;
+  enemy: Playable;
+  fg: Sprite;
+  state: State;
+  canvas: HTMLCanvasElement;
+};
 
-const motionControl = ({ ctx, state, bg, player, enemy, fg, colliders }: MotionControlArgs): void => {
-    const { keys } = state;
-    let futureKeyState: coordinates = {x: 0, y: 0};
-    let follow = false;
-    if (keys.isPressed(keys.up)) {
-        futureKeyState.y = PLAYER_MOVESPEED;
-    } else if (keys.isPressed(keys.left)) {
-        futureKeyState.x = PLAYER_MOVESPEED;
-    } else if (keys.isPressed(keys.down)) {
-        futureKeyState.y = -PLAYER_MOVESPEED;
-    } else if (keys.isPressed(keys.right)) {
-        futureKeyState.x = -PLAYER_MOVESPEED;
+const motionControl = ({
+  ctx,
+  state,
+  bg,
+  player,
+  enemy,
+  fg,
+  colliders,
+}: MotionControlArgs): void => {
+  const { keys } = state;
+  let futureKeyState: coordinates = { x: 0, y: 0 };
+  let follow = false;
+  if (keys.isPressed(keys.up)) {
+    futureKeyState.y = PLAYER_MOVESPEED;
+  } else if (keys.isPressed(keys.left)) {
+    futureKeyState.x = PLAYER_MOVESPEED;
+  } else if (keys.isPressed(keys.down)) {
+    futureKeyState.y = -PLAYER_MOVESPEED;
+  } else if (keys.isPressed(keys.right)) {
+    futureKeyState.x = -PLAYER_MOVESPEED;
+  }
+
+  const playerCollisions = colliders.some((collider: BoxCollider) =>
+    checkCollision(player, collider, futureKeyState)
+  );
+  const enemyCollisions = colliders.some((collider: BoxCollider) =>
+    checkCollision(enemy, collider, futureKeyState)
+  );
+  const enemyDistance = manhattanDistance(player.position, enemy.position);
+
+  const moveMobile = (mobile: BoxCollider | Sprite) => {
+    for (const key of Object.keys(keys.pressed)) {
+      if (!keys.keysToDirectionMap.hasOwnProperty(key)) {
+        break;
+      }
+      if (keys.isPressed(key)) {
+        player.animate(keys.keysToDirectionMap[key]);
+
+        if (!playerCollisions) {
+          const [axis, velocity] = keys.motion[key];
+          if (hasKey(mobile.position, axis)) {
+            mobile.position[axis] += velocity;
+            break;
+          }
+        }
+      }
     }
+  };
 
-    const playerCollisions = colliders.some((collider: BoxCollider) => checkCollision(player, collider, futureKeyState));
-    const enemyCollisions = colliders.some((collider: BoxCollider) => checkCollision(enemy, collider, futureKeyState));
-    const enemyDistance = manhattanDistance(player.position, enemy.position);
+  moveMobile(player);
+  if (enemyDistance <= ENEMY_CHASE_DISTANCE || follow) {
+    enemy.follow(player, colliders);
+    follow = true;
+  }
 
-    const moveMobile = (mobile: BoxCollider | Sprite) => {
-        for (const key of Object.keys(keys.pressed)) {
-            if (!keys.keysToDirectionMap.hasOwnProperty(key)) {
-                break;
-            }
-            if (keys.isPressed(key)) {
-                player.animate(keys.keysToDirectionMap[key]);
-            
-                if (!playerCollisions) {
-                    const [axis, velocity] = keys.motion[key];
-                    if (hasKey(mobile.position, axis)) {
-                        mobile.position[axis] += velocity;
-                        break;
-                    }
-                }
-            }
-        };
-    }
+  if (checkCollision(enemy, player, { x: 0, y: 0 }, -16)) {
+    enemy.attack(player, "y", ctx);
+    player.attack(enemy, "y", ctx);
+  }
+};
 
-    moveMobile(player);
-    if (enemyDistance <= ENEMY_CHASE_DISTANCE || follow) {
-        enemy.follow(player, colliders);
-        follow = true;
-    }
+export const animationBuilder = ({
+  bg,
+  player,
+  state,
+  canvas,
+  fg,
+  enemy,
+}: AnimationBuilderArgs): (() => void) => {
+  const ctx: CanvasRenderingContext2D = getCtx(canvas);
+  const sprites: Sprite[] = [bg, player, fg, enemy];
+  sprites.forEach((sprite) => sprite.loadImage(ctx));
+  const colliders = getColliders();
 
-    if (checkCollision(enemy, player, { x: 0, y: 0 }, -16)) {
-        enemy.attack(player, true).drawAttackEffects(ctx, player);
-        player.attack(enemy, keys.isPressed("y"));
-    }
-}
-
-export const animationBuilder = ({ bg, player, state, canvas, fg, enemy }: AnimationBuilderArgs): () => void => {
-    const ctx: CanvasRenderingContext2D = getCtx(canvas);
-    const sprites: Sprite[] = [bg, player, fg, enemy];
-    sprites.forEach(sprite => sprite.loadImage(ctx));
-    const colliders = getColliders();
-
-    const animate = (): void => {
-        ctx.fillStyle = "#000";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        bg.draw(ctx);
-        player.draw(ctx);
-        enemy.draw(ctx);
-        fg.draw(ctx);
-        window.requestAnimationFrame(animate);
-        motionControl({ ctx, state, bg, player, enemy, fg, colliders });
-    }
-    return animate;
-}
+  const animate = (): void => {
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    bg.draw(ctx);
+    player.draw(ctx);
+    enemy.draw(ctx);
+    fg.draw(ctx);
+    window.requestAnimationFrame(animate);
+    motionControl({ ctx, state, bg, player, enemy, fg, colliders });
+  };
+  return animate;
+};
--- src/sprites/pathfinder.ts
+++ src/sprites/pathfinder.ts
@@ -1,92 +1,100 @@
-import { TILES, DIRECTIONS } from '../constants';
-import { BoxCollider, coordsAbsPosition } from '../render/collider';
-import { makeGrid } from '../render/collider/collisions';
-import { manhattanDistance } from '../utils/misc';
-import { PriorityQueue } from '../utils/priorityQueue';
+import { TILES, DIRECTIONS } from "../constants";
+import { BoxCollider, coordsAbsPosition } from "../render/collider";
+import { makeGrid } from "../render/collider/collisions";
+import { manhattanDistance } from "../utils/misc";
+import { PriorityQueue } from "../utils/priorityQueue";
 
-
-const encodeCoords = (coordinates: coordinates) => `${coordinates.x}-${coordinates.y}`;
-
+const encodeCoords = (coordinates: coordinates) =>
+  `${coordinates.x}-${coordinates.y}`;
 
 export class PathFinder {
-    visited: Set<string>;
-    surroundings: PriorityQueue<AStarRecord>;
-    paths: TileMap;
+  visited: Set<string>;
+  surroundings: PriorityQueue<AStarRecord>;
+  paths: TileMap;
 
-    constructor () {
-        this.paths = {};
-        this.visited = new Set();
-        this.surroundings = new PriorityQueue();
-    }
-    
-    isSameCoords(c1: coordinates, c2: coordinates): boolean {
-        return c1.x === c2.x && c1.y === c2.y;
-    }
+  constructor() {
+    this.paths = {};
+    this.visited = new Set();
+    this.surroundings = new PriorityQueue();
+  }
 
-    buildPath(key: string): coordinates[] {
-        if (!this.paths.hasOwnProperty(key)) {
-            return [];
-        }
-        const path: coordinates[] = [];
-        let current = this.paths[key];
-        while (current.parent) {
-            path.push(current.position);
-            current = this.paths[encodeCoords(current.parent)];
-        }
-        return path.reverse();
-    }
+  isSameCoords(c1: coordinates, c2: coordinates): boolean {
+    return c1.x === c2.x && c1.y === c2.y;
+  }
 
-    find(source: coordinates, destination: coordinates, colliders: BoxCollider[]): coordinates[] {
-        const srcKey = encodeCoords(source);
-        let path: coordinates[] = [];
-        const aStarRecord: AStarRecord = {
-            fScore: 0,
-            gScore: 0,
-            hScore: 0,
-            parent: null,
-            position: source,
-            key: srcKey
+  buildPath(key: string): coordinates[] {
+    if (!this.paths.hasOwnProperty(key)) {
+      return [];
+    }
+    const path: coordinates[] = [];
+    let current = this.paths[key];
+    while (current.parent) {
+      path.push(current.position);
+      current = this.paths[encodeCoords(current.parent)];
+    }
+    return path.reverse();
+  }
+
+  find(
+    source: coordinates,
+    destination: coordinates,
+    colliders: BoxCollider[]
+  ): coordinates[] {
+    const srcKey = encodeCoords(source);
+    let path: coordinates[] = [];
+    const aStarRecord: AStarRecord = {
+      fScore: 0,
+      gScore: 0,
+      hScore: 0,
+      parent: null,
+      position: source,
+      key: srcKey,
+    };
+    this.visited.add(srcKey);
+    this.paths[srcKey] = aStarRecord;
+    this.surroundings.nq(0, aStarRecord);
+
+    while (!this.surroundings.empty()) {
+      const current: AStarRecord = this.surroundings.dq();
+      this.paths[current.key] = current;
+
+      if (this.isSameCoords(current.position, destination)) {
+        return this.buildPath(current.key);
+      }
+
+      for (let direction of DIRECTIONS) {
+        const [horizontal, vertical] = direction;
+        const newCoordinates = {
+          x: current.position.x + horizontal,
+          y: current.position.y + vertical,
         };
-        this.visited.add(srcKey);
-        this.paths[srcKey] = aStarRecord;
-        this.surroundings.nq(0, aStarRecord);
+        const newKey = encodeCoords(newCoordinates);
+        const isBoundary = colliders.some((collider: BoxCollider) =>
+          this.isSameCoords(
+            newCoordinates,
+            coordsAbsPosition(collider.position)
+          )
+        );
 
-        while (!this.surroundings.empty()) {
-            const current: AStarRecord = this.surroundings.dq();
-            this.paths[current.key] = current;
-
-            if (this.isSameCoords(current.position, destination)) {
-                return this.buildPath(current.key);
-            }
-
-            for (let direction of DIRECTIONS) {
-                const [horizontal, vertical] = direction;
-                const newCoordinates = { x: current.position.x + horizontal, y: current.position.y + vertical };
-                const newKey = encodeCoords(newCoordinates);
-                const isBoundary = colliders.some(
-                    (collider: BoxCollider) => this.isSameCoords(newCoordinates, coordsAbsPosition(collider.position))
-                );
-
-                if (isBoundary || this.visited.has(newKey)) {
-                    continue;
-                }
-
-                const gScore = current.gScore + 1;
-                const hScore = manhattanDistance(newCoordinates, destination);
-                const fScore = gScore + hScore;
-
-                this.visited.add(newKey);
-                this.surroundings.nq(fScore, {
-                    fScore,
-                    gScore,
-                    hScore,
-                    parent: current.position,
-                    position: newCoordinates,
-                    key: newKey
-                })
-            }
+        if (isBoundary || this.visited.has(newKey)) {
+          continue;
         }
-        return path;
-    }
 
+        const gScore = current.gScore + 1;
+        const hScore = manhattanDistance(newCoordinates, destination);
+        const fScore = gScore + hScore;
+
+        this.visited.add(newKey);
+        this.surroundings.nq(fScore, {
+          fScore,
+          gScore,
+          hScore,
+          parent: current.position,
+          position: newCoordinates,
+          key: newKey,
+        });
+      }
+    }
+    return path;
+  }
 }
